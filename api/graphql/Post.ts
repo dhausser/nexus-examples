71911b85c2ba67fa63789963abee6546dcd9ea48
Davy Hausser <davy.hausser@gmail.com>
2020-08-20 14:49:09 +0200

    :recylcle: schema tests

--- api/graphql/Post.ts
+++ api/graphql/Post.ts
@@ -19,5 +19,47 @@ schema.extendType({
         return ctx.db.post.findMany({ where: { published: false } })
       },
     })
+    t.list.field('posts', {
+      type: 'Post',
+      resolve(_root, _args, ctx) {
+        return ctx.db.post.findMany({ where: { published: true } })
+      },
+    })
+  },
+})
+
+schema.extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.field('createDraft', {
+      type: 'Post',
+      args: {
+        title: schema.stringArg({ required: true }),
+        body: schema.stringArg({ required: true }),
+      },
+      resolve(_root, args, ctx) {
+        const draft = {
+          title: args.title,
+          body: args.body,
+          published: false,
+        }
+        return ctx.db.post.create({ data: draft })
+      },
+    })
+
+    t.field('publish', {
+      type: 'Post',
+      args: {
+        draftId: schema.intArg({ required: true }),
+      },
+      resolve(_root, args, ctx) {
+        return ctx.db.post.update({
+          where: { id: args.draftId },
+          data: {
+            published: true,
+          },
+        })
+      },
+    })
   },
 })
