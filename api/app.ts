f62805a43ad9b9a3741cbda28e9a8b50bf7bd56f
Davy Hausser <davy.hausser@gmail.com>
2020-08-20 14:49:09 +0200

    :recycle: schema tests

--- api/app.ts
+++ api/app.ts
@@ -1,21 +1,12 @@
-import * as Prisma from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { on, schema } from 'nexus'
-
-/**
- * Without the Prisma plugin you need to manually expose Prisma types into your backing types.
- */
-
-// todo https://github.com/graphql-nexus/nexus/issues/473#issuecomment-665171477
-export type User = Prisma.User
-export type Post = Prisma.Post
-export type Profile = Prisma.Profile
 
 on.start(() => {
   /**
    * Without the Prisma plugin you need to manaully contruct your Prisma client instance.
    * With the plugin you can also still create your Prisma client instance if you need to.
    */
-  const db = new Prisma.PrismaClient()
+  const db = new PrismaClient()
 
   /**
    * Without the Prisma plugin you need to manually expose your Prisma client on the context.
